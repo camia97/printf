c9a01e3a6577acab18a8d90ca3a72e652eaf16d5
camia97 <3962@holbertonschool.com>
2021-11-08 10:23:28 -0800

    change comments betty

--- main.h
+++ main.h
@@ -14,22 +14,6 @@ typedef struct printable
 	char *op_op;
 	int (*f)(va_list ele);
 } pstruct;
-/*#define pstruct artype[] = {
-	{"c", _putchar}
-	{"s", op_pstring}
-	{"d",op_pdecimal}
-	{"i", op_pinterger}
-	{"b", op_pbinary}
-	{"u", op_punsign}
-	{"o", op_poctal}
-	{"x", op_phexalow}
-	{"X", op_phexaup}
-	{"+", op_pplus}
- 	{"#", op_palt}
-	{"l", op_pl}
-	{"h", op_ph}
-	{NULL,NULL}
-};*/
 int op_pchar(va_list a);
 int _printf(const char *format, ...);
 char *str_concat(char *s1, char *s2);
