4ce7888ab37b2e600984c84befd3df511f8e6d23
camia97 <3962@holbertonschool.com>
2021-11-12 14:09:36 -0800

    change function binary

--- p_fun2.c
+++ p_fun2.c
@@ -11,36 +11,6 @@ int op_pdig(va_list ele)
 	return (dig(aux_pdig));
 }
 /**
- * op_pbin - print a binary
- * @ele: list of arguments
- * Return: function
- */
-int op_pbin(va_list ele)
-{
-	int aux_pbin = va_arg(ele, int);
-
-	return (rec_bin(aux_pbin));
-}
-/**
- * rec_bin - change a binary
- * @n: int
- * Return: larg
- */
-int rec_bin(unsigned int n)
-{
-	unsigned int larg = 0;
-
-	if (n >= 1)
-	{
-		larg += rec_bin(n / 2);
-		_putchar(n % 2 + '0');
-		larg++;
-	}
-	else
-		return (-1);
-	return (larg);
-}
-/**
  * op_prev - reverse a string
  * @ele: list of arguments
  * Return: length of string
